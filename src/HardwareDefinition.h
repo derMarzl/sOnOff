0757b2412793efc803a6c0d29657682101ee992d
derMarzl <der_Marzl@gmx.de>
2018-01-10 23:10:13 +0100

    uploadport aus Umgebungsvariable

--- src/HardwareDefinition.h
+++ src/HardwareDefinition.h
@@ -4,6 +4,7 @@
 
 #include "Arduino.h"
 
+
 //#define Serialport_benutzen // auskommentieren wenn der Serialport nicht benutzt werden soll
 
 #define LED 1 // ESP01-LED kann nidht in Verbindung mit Serial benutzt werden -> Pinbelegung prüfen, gleiche Pin wie RX oder TX
